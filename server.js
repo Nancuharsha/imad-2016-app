97ddaec46d5e70b7788bdee0659eaa34ae0eb798
Nancuharsha <bandaru.harshavardhan17@gmail.com>
2016-09-22 12:46:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ var htmltemplate =`
 `;
     return htmltemplate;
 }
-app.get(':articleName',function(req,res){
+app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 
